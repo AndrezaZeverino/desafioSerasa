c0f85b3d0f22bf3da28e43384662f39fa01c224d
Andreza Zeverino <82824036+AndrezaZeverino@users.noreply.github.com>
2021-06-20 10:59:02 -0300

    Update informacoesPessoais.js

--- informacoesPessoais.js
+++ informacoesPessoais.js
@@ -6,7 +6,4 @@ button.addEventListener("click", () => {
   finalizado.innerHTML += `Parabéns, você finalizou seu empréstimo com a empresa CréditoParaTodxs, em até 4 horas o valor estará em sua conta`;
 });
 
-$(document).ready(function () {
-  let meuCampoCPF = "#CPF";
-  meuCampoCPF.mask("000.000.000-00", { reverse: true });
-});
+
